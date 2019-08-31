82ea5c4826444549cdbd09501955b44b87f0d800
S-aiueo32 <18kmc06@ms.dendai.ac.jp>
2019-09-01 00:34:54 +0900

    :recycle: path resolution for notebooks

--- models/dist_model.py
+++ models/dist_model.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 from IPython import embed
 
 from . import networks_basic as networks
-import models as util
+from PerceptualSimilarity import models as util
 
 class DistModel(BaseModel):
     def name(self):
